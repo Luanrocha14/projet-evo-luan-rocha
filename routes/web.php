9552ed6f2b729ba430a5f0e9a6792a30c37ee6c3
Luan Rocha <luanrochasantiago@gmail.com>
2025-10-22 15:55:55 -0300

    Versão 18

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create'])->name('events.create');
+Route::get('/events/{id}', [EventController::class, 'show'])->name('events.show');
 Route::post('/events', [EventController::class, 'store']);
 
 Route::get('/contact', function () {
