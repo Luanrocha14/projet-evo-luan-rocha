02f3aa63f2a2ff786cbf6f1676b84a767950e5d8
Luan Rocha <luanrochasantiago@gmail.com>
2025-10-23 16:28:59 -0300

    versão 19

--- database/migrations/2025_10_23_163930_add_items_to_events_table.php
+++ database/migrations/2025_10_23_163930_add_items_to_events_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('events', function (Blueprint $table) {
-            $table->string('image');
+            $table->json('items')->nullable();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('events', function (Blueprint $table) {
-            $table->dropColumn('image');
+            $table->dropColumn('items');
         });
     }
 };
